5d076cde1edd86202540be936b38441f2202574f
Fabien <fabien@MacBook-Pro-de-Fabien.local>
2018-11-18 21:21:02 +0100

    FDU : redirect sign in

--- front/cobweb/src/app/sign-up/sign-up.component.ts
+++ front/cobweb/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  redirect_to_sign_in() {
+    this.router.navigate(['/sign_in']);
   }
 
 }
